2a355713fcd291833ae3fc17d5b4f3389c447a46
kleber-s-araujo <124310150+kleber-s-araujo@users.noreply.github.com>
2024-12-30 08:02:42 -0300

    Templates Added
    Adicionado os templates das aplicações Web Landing Page e Painel de Administração

---
+++ renderizai-landing-page/app/(site)/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title> Renderizaí | Plataforma de Render Expresso</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Renderizaí - Render Expresso para seu Projeto" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
